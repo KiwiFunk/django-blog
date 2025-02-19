c5253f4574d75479898119467c5878d309b2c3d8
Alex <alex_p97@live.com>
2025-02-19 11:58:54 +0000

    Set project up for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4bt9*wv1z8*rni4qfwo=^666$p1jyr@^4-0ljji8h3y%3k-j#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com',
+]
 
 
 # Application definition
