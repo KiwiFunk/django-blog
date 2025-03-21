c57204e27490ae077a9428b95ef8c5da17e8910f
Alex <alex_p97@live.com>
2025-03-21 23:04:53 +0000

    Added delete view to urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView
+from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
     path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),       #Use Primary Key to denote a specific post.
     path('new_post/', CreatePostView.as_view(), name='create_post'),            #Create a new post.
-    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),   #Update an existing post.
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),     #Update an existing post.
+    path('post/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'), #Delete an existing post.
 ]
