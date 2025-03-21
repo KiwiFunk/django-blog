be18be32067cb6e1009261477f85646bccdb20cb
Alex <alex_p97@live.com>
2025-03-21 16:19:09 +0000

    Created new view and url for  displaying specific posts in detail

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),          #Call as_view() method to return a callable view that takes a request and returns a response.
+    path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),       #Use Primary Key to denote a specific post.
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -4,6 +4,10 @@ from .models import Post
 
 # Create your views here.
 
-class HomeView(ListView):
+class HomeView(ListView):                       # ListView is used to display a list of objects.
     model = Post                                # The model that this view will use to query the database.
     template_name = 'home.html'                 # The template that this view will use to generate the HTML.
+
+class PostDetailView(DetailView):               # DetailView is used to display a single object in detail.
+    model = Post
+    template_name = 'post_details.html'         # The template that this view will use to generate the HTML.
