0559be92f08e37539f65098bb4a56fccb80a7965
Alex <alex_p97@live.com>
2025-03-21 20:50:06 +0000

    Added new post page to navbar

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,5 @@ from .views import HomeView, PostDetailView, CreatePostView
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
     path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),       #Use Primary Key to denote a specific post.
-    path('new_post/', CreatePostView.as_view(), name='new_post'),               #Create a new post.
+    path('new_post/', CreatePostView.as_view(), name='create_post'),            #Create a new post.
 ]
