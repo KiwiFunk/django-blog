e1f8a2280498dfd40d26723eb875155e9b369c04
Alex <alex_p97@live.com>
2025-03-21 15:57:06 +0000

    Updated urls to work with new class based view

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HomeView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),          #Call as_view() method to return a callable view that takes a request and returns a response.
 ]
