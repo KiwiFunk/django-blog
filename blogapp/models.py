2d183bd19e4c2d5d4d09ef22b6d81a5d6242d572
Alex <alex_p97@live.com>
2025-03-21 15:32:54 +0000

    models.model > models.Model

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Post(models.model):
+class Post(models.Model):
     title = models.CharField(max_length=255)                        # The title of the post.                
     author = models.ForeignKey(User, on_delete=models.CASCADE)      # User that created the post. When the user is deleted, their posts are also deleted to prevent orphaned data.
     body = models.TextField()                                       # The body of the post. e.g the content.
