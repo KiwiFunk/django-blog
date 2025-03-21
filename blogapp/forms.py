4db7ab14d1a35b1717c0b623c040f353c769e2a6
Alex <alex_p97@live.com>
2025-03-21 22:11:43 +0000

    Added rows property to textarea in forms.py to define box size

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -11,6 +11,14 @@ class PostForm(forms.ModelForm):            #ModelForm allows for creating form 
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Browser tab title (Optional)'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'summary': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter a brief summary of the post'}),
-            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type your post here...'}),
+            'summary': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Enter a brief summary of the post to be displayed on the feed...',
+                'rows': 4
+            }),
+            'body': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Type your post here...',
+                'rows': 14
+            }),
         }
