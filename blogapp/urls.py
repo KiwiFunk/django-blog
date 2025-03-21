345e20b3d81ee3de9907e273c5da3a53b59615b6
Alex <alex_p97@live.com>
2025-03-21 22:29:53 +0000

    Added new view to urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, CreatePostView
+from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
     path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),       #Use Primary Key to denote a specific post.
     path('new_post/', CreatePostView.as_view(), name='create_post'),            #Create a new post.
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),   #Update an existing post.
 ]
