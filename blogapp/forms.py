8dd8257541a56c817d3a212361df09f99c7f25c2
Alex <alex_p97@live.com>
2025-03-21 22:24:45 +0000

    Created update_post template

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,21 +1,24 @@
 from django import forms
 from .models import Post
 
-class PostForm(forms.ModelForm):            #ModelForm allows for creating form fields for our model.
+class PostForm(forms.ModelForm):            # ModelForm allows for creating form fields for our model.
     class Meta:
         model = Post
         fields = ['title', 'title_tag', 'author', 'summary', 'body']  
 
+        # Widgets takes a python dictionary to define the attributes of the form fields.
         # Using form-control class from Bootstrap to style the form fields.
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Browser tab title (Optional)'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            
             'summary': forms.Textarea(attrs={
                 'class': 'form-control', 
                 'placeholder': 'Enter a brief summary of the post to be displayed on the feed...',
                 'rows': 4
             }),
+
             'body': forms.Textarea(attrs={
                 'class': 'form-control', 
                 'placeholder': 'Type your post here...',
