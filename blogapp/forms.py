d1f99dd5a8a84eb03bbdbb4474399cb06f89df21
Alex <alex_p97@live.com>
2025-03-21 22:01:35 +0000

    Defined fields and values inside forms.py

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,3 +1,16 @@
 from django import forms
 from .models import Post
 
+class PostForm(forms.ModelForm):            #ModelForm allows for creating form fields for our model.
+    class Meta:
+        model = Post
+        fields = ['title', 'title_tag', 'author', 'summary', 'body']  
+
+        # Using form-control class from Bootstrap to style the form fields.
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Browser tab title (Optional)'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'summary': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter a brief summary of the post'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type your post here...'}),
+        }
