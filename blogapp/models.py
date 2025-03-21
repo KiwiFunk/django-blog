90f873a1b11563e8f72a0db4d2db9d6fe12f7498
Alex <alex_p97@live.com>
2025-03-21 15:27:08 +0000

    Created basic post model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.model):
+    title = models.CharField(max_length=255)                        # The title of the post.                
+    author = models.ForeignKey(User, on_delete=models.CASCADE)      # User that created the post. When the user is deleted, their posts are also deleted to prevent orphaned data.
+    body = models.TextField()                                       # The body of the post. e.g the content.
+    created_at = models.DateTimeField(auto_now_add=True)            # The date and time the post was created.           
+
+    def __str__(self):
+        return self.title + ' | ' + self.author                     # Assign a string representation for each post object. This will be used in the admin panel. This will show the title of the post and the author's username.
