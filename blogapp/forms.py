6b9a5ec8bfcd84e443773047f5093bd738bd0b37
Alex <alex_p97@live.com>
2025-03-22 17:03:28 +0000

    Updated create and edit forms to contain dynamic list of categories

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+categories = list(Category.objects.values_list('name', 'name'))  # Cast the QuerySet to a list of tuples. Our tuple is (database value, human-readable value).
 
 class PostForm(forms.ModelForm):            # ModelForm allows for creating form fields for our model.
     class Meta:
         model = Post
-        fields = ['title', 'title_tag', 'author', 'summary', 'body']  
+        fields = ['title', 'title_tag', 'author', 'category', 'summary', 'body']  
 
         # Widgets takes a python dictionary to define the attributes of the form fields.
         # Using form-control class from Bootstrap to style the form fields.
@@ -12,6 +14,7 @@ class PostForm(forms.ModelForm):            # ModelForm allows for creating form
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Browser tab title (Optional)'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=categories, attrs={'class': 'form-control'}),
 
             'summary': forms.Textarea(attrs={
                 'class': 'form-control', 
@@ -29,13 +32,14 @@ class PostForm(forms.ModelForm):            # ModelForm allows for creating form
 class EditForm(forms.ModelForm):            # ModelForm allows for creating form fields for our model.
     class Meta:
         model = Post
-        fields = ['title', 'title_tag', 'summary', 'body']  
+        fields = ['title', 'title_tag', 'category', 'summary', 'body']  
 
         # Widgets takes a python dictionary to define the attributes of the form fields.
         # Using form-control class from Bootstrap to style the form fields.
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Browser tab title (Optional)'}),
+            'category': forms.Select(choices=categories, attrs={'class': 'form-control'}),
             
             'summary': forms.Textarea(attrs={
                 'class': 'form-control', 
