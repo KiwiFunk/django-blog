d4eed691137a4a1496d5700368b0fe57ee5eb489
Alex <alex_p97@live.com>
2025-03-22 23:42:57 +0000

    Added view to urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, CategoryView
+from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, CategoryView, LikeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),     #Update an existing post.
     path('post/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'), #Delete an existing post.
     path('category/<str:cat>', CategoryView, name='category'),                  #Filter posts by category.
+    path('like/<int:pk>', LikeView, name='like_post'),                          #Like a post.
 ]
