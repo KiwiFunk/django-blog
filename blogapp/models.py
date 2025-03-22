c33e19fa0198366b02c10304a2bed822eab508c1
Alex <alex_p97@live.com>
2025-03-22 16:25:04 +0000

    Created catagory model for organizing posts by content type

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)                                # The name of the category.
+    
+    def __str__(self):
+        return self.name                                                    # Assign a string representation for each category object. This will be used in the admin panel.
+
 class Post(models.Model):
     title = models.CharField(max_length=255)                                # The title of the post.
     title_tag = models.CharField(max_length=100, blank=True, null=True)     # A tagline for the post. (Optional)
     author = models.ForeignKey(User, on_delete=models.CASCADE)              # User that created the post. When the user is deleted, their posts are also deleted to prevent orphaned data.
     summary = models.TextField(blank=True, null=True)                       # A brief summary of the post to be displayed in the feed. (Optional)
     body = models.TextField()                                               # The body of the post. e.g the content.
-    created_at = models.DateTimeField(auto_now_add=True)                    # The date and time the post was created.           
+    created_at = models.DateTimeField(auto_now_add=True)                    # The date and time the post was created.     
+    category = models.CharField(max_length=255, default='Uncategorized')    # The category of the post.      
 
     # Assign a string representation for each post object. This will be used in the admin panel. 
     # This will show the title of the post and the author's username.
