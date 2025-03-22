583c534d55305b3ab3682a9eb1485e8ad223f321
Alex <alex_p97@live.com>
2025-03-22 21:31:21 +0000

    Created function in HomeView to create context dictionary (Including categories)

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -10,6 +10,20 @@ class HomeView(ListView):                       # ListView is used to display a 
     model = Post                                # The model that this view will use to query the database.
     template_name = 'home.html'                 # The template that this view will use to generate the HTML.
     ordering = ['-created_at']                  # The order in which the posts will be displayed on the home page.
+
+    def get_context_data(self, *args, **kwargs):
+        """
+        Get the context data for the home page. This will include the category menu.
+        1. Since Category only has one field, we can get all the objects and store them in cat_menu.
+        2. Use super to call the parent class of HomeView (ListView) and get the context data to create a context dictionary.
+        args mostly acts as a placeholder here, but kwargs will contain data such as objects, pk, etc. (keyword arguments)
+        3. Add the cat_menu to the context dictionary.
+        4. Return the context dictionary. We can now use cat_menu to access the categories in the template.
+        """
+        cat_menu = Category.objects.all()   
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class PostDetailView(DetailView):               # DetailView is used to display a single object in detail.
     model = Post
