6a1ca01e44e787ed831c8ea941244d9c6c0a9b3a
Alex <alex_p97@live.com>
2025-03-23 21:02:13 +0000

    Created Profile page url/view/template

--- usersystem/urls.py
+++ usersystem/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView, UserEditView, PasswordsChangeView
+from .views import UserRegistrationView, UserEditView, PasswordsChangeView, UserProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html'), name='change_password'),
     path('password_success/', views.password_success, name='password_success'),
+    path('<int:pk>/profile', UserProfilePageView.as_view(), name='user_profile'),
 ]
--- usersystem/views.py
+++ usersystem/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 from .forms import RegisterForm, EditProfileForm, NewPasswordForm
 from django.contrib.auth.views import PasswordChangeView
+from blogapp.models import UserProfile
 
 # Create your views here.
+class UserProfilePageView(DetailView):
+    model = UserProfile
+    template_name = 'user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserProfilePageView, self).get_context_data(*args, **kwargs)    # Use super to call the parent class get_context_data method(detail view).
+        page_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])                # Get the user profile object with the id from the URL. Kwarg = {'pk': id}
+        context["page_user"] = page_user                                                # Add the user profile object to the context.
+        return context                                                                  # Return the context dictionary.            
+
 class UserRegistrationView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/registration.html'
