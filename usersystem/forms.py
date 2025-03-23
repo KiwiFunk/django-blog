b83c4a130bf40bb5c29372e04f5d4fa560123f6a
Alex <alex_p97@live.com>
2025-03-23 17:39:23 +0000

    Made email form optional for the sake of easier testing

--- usersystem/forms.py
+++ usersystem/forms.py
@@ -18,7 +18,7 @@ class RegisterForm(UserCreationForm):
 
 class EditProfileForm(UserChangeForm):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
     
