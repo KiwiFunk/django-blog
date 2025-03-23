21dd65728d18a755d5642f329a977f90b9c2da7e
Alex <alex_p97@live.com>
2025-03-23 10:38:08 +0000

    Created edit profile template/view/url

--- usersystem/urls.py
+++ usersystem/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, EditProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('edit_profile/', EditProfileView.as_view(), name='edit_profile'),
 ]
