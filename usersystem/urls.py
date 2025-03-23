ea345c10ddac6bc6d89fee71ef19d051bdd6fbef
Alex <alex_p97@live.com>
2025-03-23 10:51:42 +0000

    Corrected usereditview, added button to change profile to navbar

--- usersystem/urls.py
+++ usersystem/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import UserRegistrationView, EditProfileView
+from .views import UserRegistrationView, UserEditView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('edit_profile/', EditProfileView.as_view(), name='edit_profile'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 ]
--- usersystem/views.py
+++ usersystem/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from .forms import RegisterForm
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 class UserRegistrationView(generic.CreateView):
@@ -9,3 +10,7 @@ class UserRegistrationView(generic.CreateView):
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')                 # Redirect to login after successful registration.
 
+class UserEditView(generic.CreateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')                  # Redirect to home after updating the profile.
