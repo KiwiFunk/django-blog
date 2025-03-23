2b8047195d96eb58161b424fd5184da62ebfbe60
Alex <alex_p97@live.com>
2025-03-23 13:21:16 +0000

    Added header image field to post model

--- blogapp/models.py
+++ blogapp/models.py
@@ -16,14 +16,15 @@ class Category(models.Model):
         return self.name                                                    # Assign a string representation for each category object. This will be used in the admin panel.
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)                                # The title of the post.
-    title_tag = models.CharField(max_length=100, blank=True, null=True)     # A tagline for the post. (Optional)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)              # User that created the post. When the user is deleted, their posts are also deleted to prevent orphaned data.
-    summary = models.TextField(blank=True, null=True)                       # A brief summary of the post to be displayed in the feed. (Optional)
-    body = models.TextField(blank=True, null=True)                          # The body of the post. e.g the content.
-    created_at = models.DateTimeField(auto_now_add=True)                    # The date and time the post was created.     
-    category = models.CharField(max_length=255, default='Uncategorized')    # The category of the post.
-    likes = models.ManyToManyField(User, related_name='blog_posts')         # Users that liked the post. A user can like multiple posts & a post can be liked by multiple users. == ManyToMany relationship.   
+    title = models.CharField(max_length=255)                                    # The title of the post.
+    title_tag = models.CharField(max_length=100, blank=True, null=True)         # A tagline for the post. (Optional)
+    header_image = models.ImageField(null=True, blank=True, upload_to='images/')# The header image for the post. (Optional)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)                  # User that created the post. When the user is deleted, their posts are also deleted to prevent orphaned data.
+    summary = models.TextField(blank=True, null=True)                           # A brief summary of the post to be displayed in the feed. (Optional)
+    body = models.TextField(blank=True, null=True)                              # The body of the post. e.g the content.
+    created_at = models.DateTimeField(auto_now_add=True)                        # The date and time the post was created.     
+    category = models.CharField(max_length=255, default='Uncategorized')        # The category of the post.
+    likes = models.ManyToManyField(User, related_name='blog_posts')             # Users that liked the post. A user can like multiple posts & a post can be liked by multiple users. == ManyToMany relationship.   
     dislikes = models.ManyToManyField(User, related_name='blog_posts_dislikes') # Users that disliked the post.
 
     def total_likes(self):
