598c83959cab51e07c3337a5cd5eb0ae6554e5cc
Alex <alex_p97@live.com>
2025-03-23 21:52:05 +0000

    Created edit profile page url

--- usersystem/urls.py
+++ usersystem/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView, UserEditView, PasswordsChangeView, UserProfilePageView
+from .views import UserRegistrationView, UserEditView, PasswordsChangeView, UserProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html'), name='change_password'),
     path('password_success/', views.password_success, name='password_success'),
     path('<int:pk>/profile', UserProfilePageView.as_view(), name='user_profile'),
+    path('<int:pk>/edit_profile_page', EditProfilePageView.as_view(), name='edit_profile_page'),
 ]
