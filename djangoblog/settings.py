93e2923f8ec607fc507e40a5407c2011d6aebe37
Alex <alex_p97@live.com>
2025-03-24 14:29:30 +0000

    Disabled debug for production

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qc!ija1#je^636rm(!bm$37bdnaidf0gqohq8ks-ec6%hnvl8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
