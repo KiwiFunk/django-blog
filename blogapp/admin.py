2adb5bec30e680810ed41107df5286c19b4579c5
Alex <alex_p97@live.com>
2025-03-24 12:28:42 +0000

    Registered featured post model in admin

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Category, UserProfile, Comment
+from .models import Post, Category, UserProfile, Comment, FeaturedPost
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
+admin.site.register(FeaturedPost)
