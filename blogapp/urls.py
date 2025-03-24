d71695384fabd87c8eca089a0b78988213c94846
Alex <alex_p97@live.com>
2025-03-24 00:12:21 +0000

    Refactored current like/dislike views to be more specific, created new ones for comment likes/dislikes

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, CategoryView, LikeView, DislikeView
+from .views import HomeView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, CategoryView, PostLikeView, PostDislikeView, CommentLikeView, CommentDislikeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),                                  #Call as_view() method to return a callable view that takes a request and returns a response.
-    path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),       #Use Primary Key to denote a specific post.
-    path('new_post/', CreatePostView.as_view(), name='create_post'),            #Create a new post.
-    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),     #Update an existing post.
-    path('post/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'), #Delete an existing post.
-    path('category/<str:cat>', CategoryView, name='category'),                  #Filter posts by category.
-    path('like/<int:pk>', LikeView, name='like_post'),                          #Like a post.
-    path('dislike/<int:pk>', DislikeView, name='dislike_post'),                 #Dislike a post.
+    path('', HomeView.as_view(), name='home'),                                      #Call as_view() method to return a callable view that takes a request and returns a response.
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),           #Use Primary Key to denote a specific post.
+    path('new_post/', CreatePostView.as_view(), name='create_post'),                #Create a new post.
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),         #Update an existing post.
+    path('post/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),     #Delete an existing post.
+    path('category/<str:cat>', CategoryView, name='category'),                      #Filter posts by category.
+    path('post/like/<int:pk>', PostLikeView, name='post_like'),                     #Like a post.
+    path('post/dislike/<int:pk>', PostDislikeView, name='post_dislike'),            #Dislike a post.
+    path('comment/like/<int:pk>', CommentLikeView, name='comment_like'),            #Like a comment.
+    path('comment/dislike/<int:pk>', CommentDislikeView, name='comment_dislike'),   #Dislike a comment.
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post, Category
+from .models import Post, Category, Comment
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy, reverse
 
@@ -42,7 +42,7 @@ def CategoryView(request, cat):
         'category_posts': category_posts
     })
 
-def LikeView(request, pk):
+def PostLikeView(request, pk):
     post = get_object_or_404(Post, id=pk)       # Get the post object with the given id. (404 if not found)
     if request.user in post.likes.all():
         post.likes.remove(request.user)         # Unlike if already liked
@@ -53,7 +53,7 @@ def LikeView(request, pk):
     
     return HttpResponseRedirect(reverse('post_details', args=[str(pk)]))
 
-def DislikeView(request, pk):
+def PostDislikeView(request, pk):
     post = get_object_or_404(Post, id=pk)
     if request.user in post.dislikes.all():
         post.dislikes.remove(request.user)      # Remove dislike
@@ -63,3 +63,29 @@ def DislikeView(request, pk):
             post.likes.remove(request.user)     # Remove like if exists
     
     return HttpResponseRedirect(reverse('post_details', args=[str(pk)]))
+
+def CommentLikeView(request, pk):
+    comment = get_object_or_404(Comment, id=pk)
+    if request.user in comment.likes.all():
+        comment.likes.remove(request.user)
+    else:
+        comment.likes.add(request.user)
+        # Remove dislike if exists
+        if request.user in comment.dislikes.all():
+            comment.dislikes.remove(request.user)
+    
+    # Redirect back to the post detail page
+    return HttpResponseRedirect(reverse('post_details', args=[str(comment.post.pk)]))
+
+def CommentDislikeView(request, pk):
+    comment = get_object_or_404(Comment, id=pk)
+    if request.user in comment.dislikes.all():
+        comment.dislikes.remove(request.user)
+    else:
+        comment.dislikes.add(request.user)
+        # Remove like if exists
+        if request.user in comment.likes.all():
+            comment.likes.remove(request.user)
+    
+    # Redirect back to the post detail page
+    return HttpResponseRedirect(reverse('post_details', args=[str(comment.post.pk)]))
